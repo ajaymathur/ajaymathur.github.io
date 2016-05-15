e231d0ed859eb6a3f596720cb048efd2b037dd9c
Ajay Narain Mathur <ajaynarainmathur@gmail.com>
2016-05-15 21:31:49 +1000

    added game

--- assets/main.js
+++ assets/main.js
@@ -8,9 +8,6 @@ $(document).ready(function(){
 	var temp = [$(".cloud").eq(0).css("left"),$(".cloud").eq(1).css("left"),$(".cloud").eq(2).css("left")];
 	
 	var temp1 = [(parseInt(temp[0].substring(0,temp[0].length-2))),(parseInt(temp[1].substring(0,temp[1].length-2))),(parseInt(temp[2].substring(0,temp[2].length-2)))];
-	
-	
-	
 	//animation for clouds
 	function step(timestamp) {
 	 for(var i=0; i<= 2;i++){
@@ -21,7 +18,6 @@ $(document).ready(function(){
 	 }
 	    window.requestAnimationFrame(step);
 	}
-
 	window.requestAnimationFrame(step);
 	
 	//animation for boat
@@ -30,31 +26,21 @@ $(document).ready(function(){
 	  function animateNext(timeStamp){	    
 	    var temp = $("#boat").css("left");    
 	    newLeft = parseInt(temp.substring(0,(temp.length-2)))+1;
-	    if(newLeft >= $(window).width())
-		{
+	    if(newLeft >= $(window).width()){
 			newLeft = -100;
 		}
 		$("#boat").css({"left" : newLeft+"px" });
-	    
 	     window.requestAnimationFrame(animateNext);
-	    
 	  }
 	  window.requestAnimationFrame(animateNext);
-	  	  
-	 /* var ocean = document.getElementById("sea"),
-	    waveWidth = 10,
-	    waveCount = Math.floor(window.innerWidth/waveWidth),
-	    docFrag = document.createDocumentFragment();
-
-		for(var i = 0; i < waveCount; i++){
-		  var wave = document.createElement("div");
-		  wave.className += " wave";
-		  docFrag.appendChild(wave);
-		  wave.style.left = i * waveWidth + "px";
-		  wave.style.webkitAnimationDelay = (i/100) + "s";
-		}
-		
-		ocean.appendChild(docFrag);*/
-	 
-	 
+	  
+	var ifrmParent = $("#testingFrame");
+	  $("#tryGameBtn").on("click",function(){
+		 ifrmParent.find("iframe").attr("src", "partials/bounce/index.html");
+		 ifrmParent.show();
+	  });
+	  $("#closeFrame").on("click",function(){
+		 ifrmParent.find("iframe").attr("src", "");
+		 ifrmParent.hide();
+	  });
 });

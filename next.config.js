f9643480f3b15f7b0323ad5a37c6e61a345bdd47
Ajay Narain Mathur <ajaynarainmathur@gmail.com>
2020-04-27 19:35:21 +0530

    remove out folder

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 const withSass = require('@zeit/next-sass');
 const withMDX = require('@next/mdx')({
     extension: /\.mdx?$/
-  })
+  });
+
+  const isProd = process.env.NODE_ENV === 'production'
+  
   module.exports = withSass(withMDX({
+    // Use the CDN in production and localhost for development.
+    assetPrefix: isProd ? '/' : '',
     pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx']
   }));
--- out/_next/static/chunks/styles.7c6de546e1777ea5c592.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{Ha1B:function(n,o,w){},rJRk:function(n,o,w){}}]);

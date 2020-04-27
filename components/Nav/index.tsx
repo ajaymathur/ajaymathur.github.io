4cd6751b03407b03aa1fd36d5bef8fa7f254fa45
Ajay Narain Mathur <ajaynarainmathur@gmail.com>
2020-04-27 21:46:40 +0530

    add mobile nav

--- components/Nav/index.tsx
+++ components/Nav/index.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import './styles.scss';
 
-function Nav() {
+function DesktopNav() {
 	return <nav>
 		<Link href="/">
 			<a>🏡 Home</a>
@@ -24,6 +25,38 @@ function Nav() {
 			<a>👥 Connect</a>
 		</Link>
 	</nav>
+}
+
+function MobileNav() {
+	const [isMenuOpen, toggleMenuState] = useState(false);
+	return <div className="mobile-nav">
+		<div className="hamburger-icon" onClick={() => toggleMenuState(!isMenuOpen)}>🍔</div>
+		<nav className={`${isMenuOpen ? 'menu-open' : 'menu-closed'}`}>
+			<Link href="/">
+				<a>🏡 Home</a>
+			</Link>
+			<Link href="/notes">
+				<a>📝 Notes</a>
+			</Link>
+			<Link href="/blogs">
+				<a>🔈 Blogs</a>
+			</Link>
+			<Link href="/projects">
+				<a>📈 Projects</a>
+			</Link>
+			<Link href="/connect">
+				<a>👥 Connect</a>
+			</Link>
+		</nav>
+	</div>
+
+}
+
+function Nav() {
+	const isMobile = useMediaQuery(
+		'only screen and (max-device-width: 480px)'
+	)
+	return isMobile ? <MobileNav /> : <DesktopNav />
 }
 
 export default Nav;
